814e1eafa970c4ef0fbd76e480772d609cb45bab
SVingelsgaard <94757509+SVingelsgaard@users.noreply.github.com>
2022-05-24 22:00:02 +0200

    Add files via upload

--- mega.ino
+++ mega.ino
@@ -4,8 +4,14 @@
 SoftwareSerial BTSerial(12, 13);
 
 //variables
-byte data;  // serial write sends bytes.
+byte data;
+int data1;  // serial write sends bytes.
+int data2;
+int data3;
+int data4;
+
 int pot;
+int datacount = 0;
 
 void setup()
 {
@@ -22,12 +28,39 @@ void loop()
   if (millis() - timer >= interval)
   {
     timer = millis();
-    pot = map(analogRead(A0), 0, 1023, 0, 100);
-    data = pot;
+    pot = map(analogRead(A0), 0, 1023, 0, 50);
+    data1 = pot;
+    data2 = pot + 64;
+    data3 = pot + 128;
+    data4 = pot + 192;
+    
     //communication
+    if (datacount == 0){
+      data = data1;
+    }else if (datacount == 1){
+      data = data2;
+    }else if (datacount == 2){
+      data = data3;
+    }else if (datacount == 3){
+      data = data4;
+    }
 
-    Serial.println(data);
-
+    datacount = datacount + 1;
+    if (datacount > 3){
+      datacount = 0;
+    }
+    Serial.print("1: ");
+    Serial.println(data1);
+    Serial.print("2: ");
+    Serial.println(data2);
+    Serial.print("3: ");
+    Serial.println(data3);
+    Serial.print("4: ");
+    Serial.println(data4);
+  
   }
+  //data = 
   BTSerial.write(data);
+  //Serial.println(data);
+  
 }
--- uno.ino
+++ uno.ino
@@ -6,6 +6,11 @@ SoftwareSerial BTSerial(11, 12);
 
 //variables
 int data;
+int data1;
+int data2;
+int data3;
+int data4;
+
 void setup() {
   BTSerial.begin(38400);
   Serial.begin(115200);
@@ -16,14 +21,36 @@ void loop() {
 
   if (BTSerial.available() > 0) {
     data = int(BTSerial.read());
+    if (data > 192){
+      data4 = data;
+    }else if(data > 128){
+      data3 = data;
+    }else if(data > 64){
+      data2 = data;
+    }else{
+      data1 = data;
+    }
 
   }
-
+  //Serial.print("data1: ");
+  Serial.print(" ");
+  Serial.print(data1);
+  //Serial.print("data2: ");
+  Serial.print(" ");
+  Serial.print(data2);
+  //Serial.print("data3: ");
+  Serial.print(" ");
+  Serial.print(data3);
+  //Serial.print("data4: ");
+  Serial.print(" ");
+  Serial.print(data4);
+  
   static unsigned long timer = 0;
   unsigned long interval = 10; // send 100 samples/second
   if (millis() - timer >= interval)
   {
     timer = millis();
-    Serial.println(data);
+    //Serial.print("data1: ");
+    
   }
 }
